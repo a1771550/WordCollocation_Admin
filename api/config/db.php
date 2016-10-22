2a581d4f28112f4a26d42f137711c563ab2947d8
Kevin Lau <a1771550@gmail.com>
2016-10-23 02:40:31 +0800

    Update db.php

--- api/config/db.php
+++ api/config/db.php
@@ -2,11 +2,5 @@
 
 return [
 	'class' => 'yii\db\Connection',
-	/*'dsn' => 'mysql:host=translationhall.com;dbname=kevinl57_wordcollocation',
-	'username' => 'kevinl57_admin',
-	'password' => '918273',*/
-	'dsn'=>'mysql:host=localhost; dbname=wordcollocation',
-	'username'=>'root',
-	'password'=>'111111',
-	'charset' => 'utf8',
+	
 ];
